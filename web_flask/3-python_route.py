b514f02b7f64d6c73bd3cd6eb1a213dba97eaec1
FelixGathuo <gathuofelix@gmail.com>
2022-10-15 12:45:18 +0000

    web flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,6 +24,11 @@ def c(text):
 	text= text.replace("_", " ")
 	return "C {}".format(text)
 
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+	text = text.replace("_", " ")
+	return "Python {}".format(text)
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0")
