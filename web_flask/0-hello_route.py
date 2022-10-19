2910be2aed86091fe75038fcd922073afe3e0378
FelixGathuo <gathuofelix@gmail.com>
2022-10-19 15:11:26 +0000

    web flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 def hello_route():
 	return 'Hello HBNB!'
 
-if __name__ == "__main__":
-	app.run(host="0.0.0.0", port="5000"
-			)
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port='5000')
+
